436b35ffe3067356a35a9a87c9eca1e6663d4082
ethannance <119529190+ethannance@users.noreply.github.com>
2023-03-02 09:01:00 -0700

    Update master_burger.py

--- master_burger.py
+++ master_burger.py
@@ -1,30 +1,51 @@
-# Nya Croft
+import random
 
-from random import randint
-
-class Order :
-    def __init__ (self) :
+class Order () :
+    def __init__(self) :
         self.burger_count = self.randomBurgers()
+    
+    def randomBurgers(self) :
+        self.random_burger = random.randint(0,20)
+        return self.random_burger
 
-    def randomBurgers (self) :
-        iRandomNum = randint (1,20)
-        return iRandomNum
-
-class Person : 
-    def __init__ (self) : 
+class Person() :
+    def __init__(self) :    
         self.customer_name = self.randomName()
-    
+
     def randomName (self) :
+        
         asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
-        iCustomerCount = len(asCustomers)
-        iRandomCustomer = randint(0, iCustomerCount - 1)
-        return asCustomers[iRandomCustomer]
-    
-class Customer (Person):
-    def __init__ (self, oCustomer) :
+        random_name = random.randint(0,8)
+
+        CustomerName = asCustomers[random_name]
+        return CustomerName
+
+class Customer (Person) :
+    def __init__(self) :
         super().__init__()
         self.order = Order()
 
-        #queueCustomers = []
-    
-oCustomer = Person()
+queue = []
+
+customer_info = {}
+
+iCustomerCount = 100
+
+for iCount in range(1, iCustomerCount) :
+    customer = Customer()
+    queue.append(customer)
+    customer_name = customer.customer_name
+    burger_count = customer.order.burger_count
+    if customer_name in customer_info :
+        customer_info[customer_name] = customer_info[customer_name] + customer.order.burger_count
+    else:
+        customer_info[customer_name] = customer.order.burger_count
+
+listSortedCustomers = sorted(customer_info.items(), key=lambda x: x[1], reverse = True)
+
+print("\n")
+for customer in listSortedCustomers:
+    print(customer[0].ljust(19),str(customer[1]))
+print("\n")
+
+oPerson = Person()
